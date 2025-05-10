523a3c4e6fc5d487353d0cbc5b44946b4749ba82
ユウタ <qitengyuutasaitouyuuta@gmail.com>
2025-05-10 22:53:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-async function startCamera() {
-  try {
-    const constraints = {
-      video: { facingMode: { ideal: "environment" } },
-      audio: false
-    };
+const stream = await navigator.mediaDevices.getUserMedia({
+  video: { facingMode: { ideal: "environment" } },
+  audio: false
+});
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     const videoElement = document.getElementById("camera");
     videoElement.srcObject = stream;
